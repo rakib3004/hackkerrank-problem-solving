f14d028048ecec836f6d933fa7f3bc0036f850cf
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-05-18 09:44:24 +0600

    C/C++ Problem Solve 12 - 14
    C/C++ Problem Solve 12 - 14

---
+++ C Programmming/Playing With Characters.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char cw,w;
+    char a[500];
+    char p[8000];
+
+    scanf("%c",&cw);
+   scanf("%c",&w);
+
+    scanf ("%s",a);
+
+     gets(p);
+   scanf("%[^\n]%*c",&p);
+
+    printf("%c\n%s\n%s",cw,a,p);
+
+
+
+
+
+
+    return 0;
+}
+
+
